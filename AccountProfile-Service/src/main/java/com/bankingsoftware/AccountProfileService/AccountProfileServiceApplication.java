14c2f1e5f10a8739427e42e042e30a513d402055
yashsaxena1503 <saxenayash1503@gmail.com>
2021-02-12 23:11:49 +0530

    [Yash]- Adding Swagger

--- AccountProfile-Service/src/main/java/com/bankingsoftware/AccountProfileService/AccountProfileServiceApplication.java
+++ AccountProfile-Service/src/main/java/com/bankingsoftware/AccountProfileService/AccountProfileServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class AccountProfileServiceApplication {
 
 	public static void main(String[] args) {
